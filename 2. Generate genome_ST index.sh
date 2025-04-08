366d59e396ce28c8904e18151d868768bd9876e6
Anaisa Cajigas Gandia <acajigasgandia27@gmail.com>
2025-04-08 10:12:21 +0200

    Update and rename 2. Generate genome + ST index to 2. Generate genome_ST index.sh

--- 2. Generate genome_ST index.sh
+++ 2. Generate genome_ST index.sh
@@ -1,5 +1,9 @@
 ### Generation of genome index ###
 
+Author: Anaisa Cajigas Gandia
+License: MIT
+Last updated: 8.4.2025
+
 STAR --runThreadN 32 \
 --runMode genomeGenerate --genomeDir \
 --genomeFastaFiles GCA_033576555.1_ASM3357655v1_genomic.fa
