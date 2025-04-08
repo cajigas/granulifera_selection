27c408c902f78d30c36155a103ce4ef2f54ddd80
Anaisa Cajigas Gandia <acajigasgandia27@gmail.com>
2025-04-08 10:59:18 +0200

    Update 10. VCF postprocessing.sh

--- 10. VCF postprocessing.sh
+++ 10. VCF postprocessing.sh
@@ -10,26 +10,26 @@ help> https://github.com/vcftools/vcftools
 
 # Filter in vcftools
 
-path/to/vcftools --vcf granulifera_GATK_variants.filtered.SNPS.biallelic.vcf --minDP 3 --maf 0.05 --max-missing 0.75 \
+vcftools --vcf granulifera_GATK_variants.filtered.SNPS.biallelic.vcf --minDP 3 --maf 0.05 --max-missing 0.75 \
 --recode --recode-INFO-all --out granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8
 
 # Remove loci in Linkage disequilibrium (LD) with PLINK
-path/to/plink2 --vcf granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.recode.vcf \
+plink2 --vcf granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.recode.vcf \
 --allow-extra-chr --set-all-var-ids @:#\$r,\$a --indep-pairwise 50 1 0.7 --bad-ld 
 
-path/to/plink2 --vcf granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.recode.vcf \
+plink2 --vcf granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.recode.vcf \
 --allow-extra-chr --set-all-var-ids @:#\$r,\$a --exclude plink2.prune.out --make-bed \
 --recode --out granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.LDpruned.PLINK
 
 # Export in different formats
-path/to/plink --bfile granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.LDpruned.PLINK \
+plink --bfile granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.LDpruned.PLINK \
 --allow-extra-chr --recode A --out granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.LDpruned.PLINK
 
-path/to/plink --bfile granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8_noCDS.LDpruned.PLINK \
+plink --bfile granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8_noCDS.LDpruned.PLINK \
 --allow-extra-chr --recode --tab --out granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.LDpruned.PLINK
 
-path/to/plink2 --bfile granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.LDpruned.PLINK --allow-extra-chr --freq
+plink2 --bfile granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.LDpruned.PLINK --allow-extra-chr --freq
 plink2 --bfile granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.LDpruned.PLINK --allow-extra-chr --make-rel square --read-freq plink2.afreq
 
-path/to/plink2 --bfile granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.LDpruned.PLINK \
+plink2 --bfile granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.LDpruned.PLINK \
 --allow-extra-chr --export vcf --out granulifera_GATK_variants.filtered.biallelic.SNPs.minDP3mac3maxmissing8.LDpruned.PLINK 
