219dd705fa2c6c6ed56b724bf478b9e6051bfd85
Anaisa Cajigas Gandia <acajigasgandia27@gmail.com>
2025-04-08 11:11:29 +0200

    Update 7. SplitNCigar.sh

--- 7. SplitNCigar.sh
+++ 7. SplitNCigar.sh
@@ -2,24 +2,23 @@
 # The code for both references is the same, just modules change
 
 Author: Anaisa Cajigas Gandia
-License: MIT
+License: GNU
 Last updated: 8.4.2025
 
 # for the genome
 #!/bin/bash
-
 module load anaconda3/2019.03
 module load java/8
 module load gatk
 conda init bash
 
-samplesheet="granulifera_samples_merged_sorted_deduped.ok.bam.txt"
+samplesheet="path/to/file/granulifera_samples_merged_sorted_deduped.ok.bam.txt"
 threads=$SLURM_JOB_CPUS_PER_NODE
 name=`sed -n "$SLURM_ARRAY_TASK_ID"p $samplesheet | awk '{print $1}'`
 BAM=`sed -n "$SLURM_ARRAY_TASK_ID"p $samplesheet | awk '{print $2}'`
-reference="GCA_033576555.1_ASM3357655v1_genomic.fa"
-INDIR="my directory"
-OUTDIR="my directory"
+reference="path/to/reference_genome/GCA_033576555.1_ASM3357655v1_genomic.fa"
+INDIR="path/to/input_directory"
+OUTDIR="path/to/output_directory"
 DEDUPED=$OUTDIR/"${name}"_merged.sorted.deduped.ok.bam
 CIGAR=$OUTDIR/"${name}"_merged.sorted.deduped.ok.cigar.bam
 
@@ -28,6 +27,7 @@ samtools faidx -c $reference
 java -jar  picard.jar CreateSequenceDictionary R=GCA_033576555.1_ASM3357655v1_genomic.fa \
 O= GCA_033576555.1_ASM3357655v1_genomic.dict
 
+#Create directory for temporal big files that are generated during this step
 mkdir temp
 cd $OUTDIR
 gatk SplitNCigarReads --java-options '-Xmx20G -XX:+UseParallelGC -XX:ParallelGCThreads=4' \
@@ -38,7 +38,7 @@ gatk SplitNCigarReads --java-options '-Xmx20G -XX:+UseParallelGC -XX:ParallelGCT
 samtools index -c -@ 4 $CIGAR
 
 # for the SuperTranscriptome
-
+#!/bin/bash
 module load miniconda3/22.11.1
 module load gcc/13.2.0
 module load openmpi/4.1.6
