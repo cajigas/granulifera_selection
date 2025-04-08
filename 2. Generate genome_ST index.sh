29c4d3cacb29b5bc4e84e5563c6198465181ffc5
Anaisa Cajigas Gandia <acajigasgandia27@gmail.com>
2025-04-08 10:51:07 +0200

    Update 2. Generate genome_ST index.sh

--- 2. Generate genome_ST index.sh
+++ 2. Generate genome_ST index.sh
@@ -1,17 +1,18 @@
 ### Generation of genome index ###
 
 Author: Anaisa Cajigas Gandia
-License: MIT
+License: GNU
 Last updated: 8.4.2025
 
+# Change to the directory where STAR is
 STAR --runThreadN 32 \
 --runMode genomeGenerate --genomeDir \
---genomeFastaFiles GCA_033576555.1_ASM3357655v1_genomic.fa
+--genomeFastaFiles path/to/reference_genome/GCA_033576555.1_ASM3357655v1_genomic.fa
 
 
 ### Generation of SuperTranscriptome index ###
 
 STAR --runThreadN 24 --runMode genomeGenerate --genomeDir \
---genomeFastaFiles SuperDuper.sorted.fasta --genomeSAindexNbases 12 \
+--genomeFastaFiles path/to/reference_supertranscriptome/SuperDuper.sorted.fasta --genomeSAindexNbases 12 \
 --sjdbGTFfile SuperDuperTrans.sorted.gff \
 --sjdbOverhang 149 --limitGenomeGenerateRAM 100000000000
