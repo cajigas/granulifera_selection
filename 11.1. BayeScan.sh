45c83d89113b405cec68214a72b55a2be45b4f18
Anaisa Cajigas Gandia <acajigasgandia27@gmail.com>
2025-04-16 18:02:48 +0200

    Update 11.1. BayeScan.sh

--- 11.1. BayeScan.sh
+++ 11.1. BayeScan.sh
@@ -1,1 +1,5 @@
+### BayeScan analyses ###
+This script contains all the steps followed to run BayeScan, from the input file preparation in the adequate format,
+to running the software in HPC server, and then analysing results in R.
+
 
