5b53a844141b4b9148ebbb0ddd0f3ae12476d60a
Anaisa Cajigas Gandia <acajigasgandia27@gmail.com>
2026-07-14 17:22:34 +0200

    Clarify ST assembly and genome index generation
    Updated script to clarify the generation of ST assembly and genome indexes.

--- Bioinformatic_pipeline/2. ST assembly_Generate genome_ST index.sh
+++ Bioinformatic_pipeline/2. ST assembly_Generate genome_ST index.sh
@@ -1,12 +1,20 @@
-### Generation of genome index ###
+### Generation of ST assembly, and ST and genome indexes ###
 
 Author: Anaisa Cajigas Gandia
 License: GNU
 Last updated: 8.4.2025
 
+### Generation of genome index ###
 STAR --runThreadN 32 \
 --runMode genomeGenerate --genomeDir \
 --genomeFastaFiles path/to/reference_genome/GCA_033576555.1_ASM3357655v1_genomic.fa
+
+### SuperTranscriptome assembly with Lace ###
+
+/data/Software/necklace-1.01/tools/bin/lace transcript_collection.okay.mrna.annotations-no-contams.shortheaders.fasta
+/data/granulifera/all_tissues/LACE/annotations-no-contams_trancript-to-gene_mappings.tsv \
+--outputDir /data/granulifera/all_tissues/LACE/output
+--cores 24 --alternate --maxTran 500
 
 ### Generation of SuperTranscriptome index ###
 
