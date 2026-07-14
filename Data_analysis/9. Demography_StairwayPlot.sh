deabd673a7c7edc38cb555afbeb1286713c013ae
Anaisa Cajigas Gandia <acajigasgandia27@gmail.com>
2026-07-14 17:46:44 +0200

    Update 9. Demography_StairwayPlot.sh

--- Data_analysis/9. Demography_StairwayPlot.sh
+++ Data_analysis/9. Demography_StairwayPlot.sh
@@ -4,3 +4,4 @@ Author: Anaisa Cajigas Gandia
 License: GNU
 Last updated: 14.7.2026
 
+### Scripts for this analysis are available in the directory: https://github.com/cajigas/granulifera_SNPs/demography ###
